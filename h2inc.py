406adf478a344664af5adb4708799efccf452171
Lerking <33354709+Lerking@users.noreply.github.com>
2018-12-11 17:15:23 +0100

    Update h2inc.py
    Started H2INC class framework.
    Added traverse, read_file and write_file definitions.

--- h2inc.py
+++ h2inc.py
@@ -1,1 +1,11 @@
+import os
 
+class H2INC:
+    def traverse():
+        sourcedir = "/usr/src"
+        destdir = "~/include"
+        
+    def read_file(fn):
+        
+    def write_file(fn):
+        
