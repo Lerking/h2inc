6d50786773ecfb501189170d66bc9dfc2338976b
Lerking <jan.lerking@gmail.com>
2019-04-02 14:35:42 +0200

    Updated comment.py

--- comment.py
+++ comment.py
@@ -1,11 +1,15 @@
 from metaclasses import InstanceCounterMeta
+from rply import LexerGenerator
 
 class comment(object, metaclass=InstanceCounterMeta):
     def __init__(self,com):
         self.id = next(self.__class__._ids)
-        self.comment = com
-        self.block_comment = False
-        self.line_comment = False
+        self.comment_lexer = LexerGenerator()
+        self.comment_lexer.add("CSTART", r"(/*)")
+        
+        self.comment_lexer.add("CEND", r"(*/)")
+        self.comment_lexer.ignore(r'\s+')
+        
     
     def parse_comment(self):
         com = self.comment
