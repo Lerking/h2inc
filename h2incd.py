8c163e7ad4b133e049cc596690150a6ab0e4ab6c
Jan Lerking <jan.lerking@gmail.com>
2020-03-26 20:53:34 +0100

    added h2incd.py

---
+++ h2incd.py
@@ -0,0 +1,30 @@
+import sys, time
+from daemon import Daemon
+from watchdog.observers import Observer
+from watchdog.events import FileSystemEventHandler
+
+class h2incDaemon(Daemon):
+	def run(self):
+		while True:
+			time.sleep(1)
+
+class MyHandler(FileSystemEventHandler):
+    def on_modified(self, event):
+        print(f'event type: {event.event_type}  path : {event.src_path}')
+		
+if __name__ == "__main__":
+	daemon = h2incDaemon('/tmp/h2inc-daemon.pid')
+	if len(sys.argv) == 2:
+		if 'start' == sys.argv[1]:
+			daemon.start()
+		elif 'stop' == sys.argv[1]:
+			daemon.stop()
+		elif 'restart' == sys.argv[1]:
+			daemon.restart()
+		else:
+			print('Unknown command')
+			sys.exit(2)
+		sys.exit(0)
+	else:
+		print('usage: %s start|stop|restart' % sys.argv[0])
+		sys.exit(2)
